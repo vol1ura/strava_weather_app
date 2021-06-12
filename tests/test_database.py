416daf824d34b2b5f66383222d6bb33251aa999d
Yuriy Volodin <vjv@inbox.ru>
2021-06-12 14:04:36 +0300

    add tests to cover database managing methods

--- tests/test_database.py
+++ tests/test_database.py
@@ -59,8 +59,62 @@ def test_add_athlete_update(database, monkeypatch):
     assert updated_expected_tokens1 == actual_token1
 
 
-def test_add_settings():
-    pass
+def test_add_settings_no_changes(database, monkeypatch):
+    monkeypatch.setattr(manage_db, 'get_db', lambda: database)
+    # 1. Checking that we have the values with settings in database
+    cur = database.cursor()
+    record = cur.execute("SELECT * FROM settings WHERE id = 1")
+    assert expected_settings1 == manage_db.Settings(*record.fetchone())
+    # 2. Making transaction
+    manage_db.add_settings(expected_settings1)
+    # 3. Testing that there is no changes in first settings
+    record = cur.execute("SELECT * FROM settings WHERE id = 1")
+    actual_settings1 = manage_db.Settings(*record.fetchone())
+    assert actual_settings1 == expected_settings1
+
+
+def test_add_settings_with_update(database, monkeypatch):
+    monkeypatch.setattr(manage_db, 'get_db', lambda: database)
+    # 1. Checking that we have the values with id=1 in database
+    cur = database.cursor()
+    record = cur.execute("SELECT * FROM settings WHERE id = 1")
+    assert expected_settings1 == manage_db.Settings(*record.fetchone())
+    # 2. Update values in Settings object
+    expected_settings3 = expected_settings1._replace(icon=0, hum=0, wind=1, aqi=0, lan='ru')
+    manage_db.add_settings(expected_settings3)
+    # 3. Testing that changes have been applied
+    record = cur.execute("SELECT * FROM settings WHERE id = 1")
+    actual_settings3 = manage_db.Settings(*record.fetchone())
+    assert actual_settings3 == expected_settings3
+
+
+def test_add_settings_new(database, monkeypatch):
+    monkeypatch.setattr(manage_db, 'get_db', lambda: database)
+    # 1. Checking that we have no settings for such athlete_id in database
+    expected_settings2 = manage_db.Settings(2, 0, 1, 2, 3, 'ru')
+    cur = database.cursor()
+    record = cur.execute(f"SELECT * FROM settings WHERE id = {expected_settings2.id}")
+    assert record.fetchone() is None
+    # 2. Call method to test it
+    manage_db.add_settings(expected_settings2)
+    # 3. Testing that changes have been appended to database
+    record = cur.execute(f"SELECT * FROM settings WHERE id = {expected_settings2.id}")
+    actual_settings2 = manage_db.Settings(*record.fetchone())
+    assert actual_settings2 == expected_settings2
+
+
+def test_add_settings_new_default(database, monkeypatch):
+    monkeypatch.setattr(manage_db, 'get_db', lambda: database)
+    # 1. Checking that we have no settings for such athlete_id in database
+    expected_settings2 = manage_db.DEFAULT_SETTINGS._replace(id=2)
+    cur = database.cursor()
+    record = cur.execute(f"SELECT * FROM settings WHERE id = {expected_settings2.id}")
+    assert record.fetchone() is None
+    # 2. Call method to test it
+    manage_db.add_settings(expected_settings2)
+    # 3. Testing that settings with default values will not be appended to database
+    record = cur.execute(f"SELECT * FROM settings WHERE id = {expected_settings2.id}")
+    assert record.fetchone() is None
 
 
 def test_get_settings(database, monkeypatch):
@@ -83,5 +137,3 @@ def test_delete_athlete(database, monkeypatch):
     assert db_record_subcribers.fetchone() is None
     db_record_settings = cur.execute("SELECT * FROM settings WHERE id = 1")
     assert db_record_settings.fetchone() is None
-
-
